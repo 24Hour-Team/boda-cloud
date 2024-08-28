71b23a849711484de9f265ae7d3e8d9893a85914
aida <ghk01214@naver.com>
2024-08-28 18:34:10 +0900

    BODA-55 feat: S3 연결을 위한 IAM 생성 모듈 삭제
    IAM을 웹 콘솔로 사전 생성함으로써 IAM 생성 코드 삭제 및 IAM 프로필 연결 코드를 AI 모듈로 이동

--- src/Terraform/modules/IAM/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "iam_instance_profile_name" {
-  description = "The name of the IAM instance profile"
-  value       = aws_iam_instance_profile.boda_ec2_instance_profile.name
-}
--- src/Terraform/modules/ai/main.tf
+++ src/Terraform/modules/ai/main.tf
@@ -1,3 +1,8 @@
+resource "aws_iam_instance_profile" "boda_ec2-s3_instance_profile" {
+  name = var.ec2-s3_iam_instance_profile_name
+  role = var.ec2-s3_role_name
+}
+
 resource "aws_instance" "ai" {
   ami           = var.ami_ids["Amazon Linux 2023"]
   instance_type = var.instance_type
@@ -5,7 +10,7 @@ resource "aws_instance" "ai" {
   subnet_id     = var.private_subnet_ids[var.instance_indexes["ai"] - 1]
   vpc_security_group_ids = [var.private_security_group_id]
 
-  iam_instance_profile = var.iam_instance_profile_name
+  iam_instance_profile = var.ec2-s3_iam_instance_profile_name
   
   tags = {
     Name = "BODA AI"
