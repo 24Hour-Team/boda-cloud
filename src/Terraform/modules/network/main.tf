1032c379d57934877518653bf5f8be845da2edb1
aida <ghk01214@naver.com>
2024-09-10 01:18:52 +0900

    BODA-57 feat: 네트워크 az 변경

--- src/Terraform/modules/network/main.tf
+++ src/Terraform/modules/network/main.tf
@@ -11,10 +11,10 @@ resource "aws_subnet" "public" {
   count             = length(var.public_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidrs[count.index]
-  availability_zone = data.aws_availability_zones.selected.names[0]
+  availability_zone = data.aws_availability_zones.selected.names[count.index]
   
   tags = {
-    Name = count.index == 0 ? "Frontend BODA subnet" : "NAT BODA subnet"
+    Name = count.index == 0 ? "Bastion BODA subnet" : "NAT BODA subnet"
   }
 }
 
@@ -22,7 +22,7 @@ resource "aws_subnet" "private" {
   count             = length(var.private_subnet_cidrs) - 1
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidrs[count.index + 1]
-  availability_zone = data.aws_availability_zones.selected.names[0]
+  availability_zone = data.aws_availability_zones.selected.names[1]
   
   tags = {
     Name = "${var.instance_names[count.index + 1]} BODA subnet"
